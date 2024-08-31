b326fa8d1194d720d208565d8560561bbda8221f
Aminben2 <aminbenomar454@gmail.com>
2024-08-31 15:17:31 +0100

    C02--ex10

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 12:35:32 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/07/29 22:23:23 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	len = 0;
+	while (src[len])
+		len++;
+	return (len);
+}

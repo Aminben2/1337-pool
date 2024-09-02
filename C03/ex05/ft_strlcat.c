00ea9f203f02b9f9c6b04a7607f27c2963724f2a
Aminben2 <aminbenomar454@gmail.com>
2024-09-02 15:22:26 +0100

    C03--ex05

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 14:41:21 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/08/07 10:23:40 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	while (dest[dest_len])
+		dest_len++;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (size == 0)
+		return (src_len);
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && i < (size - dest_len - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}

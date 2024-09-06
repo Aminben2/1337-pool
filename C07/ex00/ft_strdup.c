d10b902654ef817e2dd59edb7ec9f71af169222d
Aminben2 <aminbenomar454@gmail.com>
2024-09-06 14:58:03 +0100

    C07--ex00

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 11:56:22 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/08/09 21:36:18 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	int		i;
+	char	*ptr;
+
+	if (src == NULL)
+		return (NULL);
+	len = 0;
+	while (src[len])
+		len++;
+	ptr = malloc(len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		ptr[i] = src[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
